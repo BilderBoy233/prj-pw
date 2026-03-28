2e6564fd67da810863a298c44640985608595a67
BilderBoy233 <rafaelcouto110@gmail.com>
2026-03-28 18:17:07 -0300

    index.php

--- prj-pw/index.php
+++ prj-pw/index.php
@@ -2,12 +2,14 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport content="width=device -width, initial-scale=1.0>
+    <meta name="viewport content="width=device -width, initial-scale=1.0">
     <meta name="keywords" content="">
     <meta name="descripition" content="Free web tutorials">
     <meta name="author" content="">
     <link rel="Icon" type="image/x-Icon" href="img/form.ico">
-    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
     <title>Form</title>
 </head>
 <body>
@@ -20,14 +22,18 @@
         
         <label for="email">Email: </label> 
         <input type="text" id="email" name="email">
-
+        
         <label for="date">Nascimento: </label>
         <input type="date" id="date" name="date">
-
+        
         <label for="file">Sua foto engraçada: </label>
         <input type="file" id="file" name="file">
-
-        <button type="submit" id="botao" name="botao">ENVIAR!!!</button>
+        
+        <!-- <button type="submit" id="botao" name="botao">ENVIAR!!!</button> -->
+        
+        
+        <button type="submit" id="enviarBtn">Enviar</button>
     </form>
+
 </body>
 </html>
